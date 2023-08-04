129f8b17ecfb8a092944696bfa21e302925f27b9
a8708b786393754ff8d101aa80623d83 <74986761+a8708b786393754ff8d101aa80623d83@users.noreply.github.com>
2023-08-04 23:29:49 +0200

    delete comment code

--- changemac.py
+++ changemac.py
@@ -4,6 +4,15 @@ import scapy.interfaces as sci
 from scapy.config import conf
 from scapy.arch import get_if_hwaddr
 
+
+if os.name != 'posix': #NOTE Linux
+    print('Your OS is not linux')
+    exit(0)
+# 
+if os.getuid() != 0:
+    print('sudo permitted! ')
+    exit(0)
+
 ifaces = sci.get_if_list()
 for iface in sci.get_if_list():
     print(iface + ': '+get_if_hwaddr(iface))
@@ -19,12 +28,4 @@ if is_in:  #NOTE non vide
     os.system(f'sudo ifconfig {choice} up')
     
 
-# if os.name != 'posix': #NOTE Linux
-#     print('Your OS is not linux')
-#     exit(0)
 
-# if os.getuid() != 0:
-#     print('sudo permitted! ')
-#     exit(0)
-
-# os.system('ifconfig')
