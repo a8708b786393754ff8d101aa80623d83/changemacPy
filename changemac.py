0202c6e093e1157ab70418e4b65330ef1341c18e
a8708b786393754ff8d101aa80623d83 <74986761+a8708b786393754ff8d101aa80623d83@users.noreply.github.com>
2023-08-04 23:27:41 +0200

    add ifaces print

--- changemac.py
+++ changemac.py
@@ -1,10 +1,24 @@
 #! /usr/bin/python3
-import os 
+import os
+import scapy.interfaces as sci
+from scapy.config import conf
+from scapy.arch import get_if_hwaddr
 
-if os.name != 'posix': #NOTE Linux
-    print('Your OS is not linux')
-    exit(0)
+ifaces = sci.get_if_list()
+for iface in sci.get_if_list():
+    print(iface + ': '+get_if_hwaddr(iface))
 
-if os.getuid() != 0: 
-    print('sudo permitted! ')
-    exit(0)
+    
+
+    
+    
+
+# if os.name != 'posix': #NOTE Linux
+#     print('Your OS is not linux')
+#     exit(0)
+
+# if os.getuid() != 0:
+#     print('sudo permitted! ')
+#     exit(0)
+
+# os.system('ifconfig')
