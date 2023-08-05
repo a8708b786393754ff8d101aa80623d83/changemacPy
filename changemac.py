24c4a740490263a4aa2373baaf770ea5a8d3ae3d
a8708b786393754ff8d101aa80623d83 <74986761+a8708b786393754ff8d101aa80623d83@users.noreply.github.com>
2023-08-05 12:58:38 +0200

    setting import, funct for get_mac_random and add the mac random in command linux

--- changemac.py
+++ changemac.py
@@ -1,31 +1,48 @@
-#! /usr/bin/python3
+#! /usr/bin/python3.10
 import os
-import scapy.interfaces as sci
-from scapy.config import conf
+import string
+import random
+
+from scapy.arch import get_if_list
 from scapy.arch import get_if_hwaddr
 
 
-if os.name != 'posix': #NOTE Linux
+if os.name != 'posix':  # NOTE Linux
     print('Your OS is not linux')
     exit(0)
-# 
+#
 if os.getuid() != 0:
     print('sudo permitted! ')
     exit(0)
 
-ifaces = sci.get_if_list()
-for iface in sci.get_if_list():
+def get_random_mac_address():
+    """Generate and return a MAC address in the format of Linux"""
+
+    # get the hexdigits uppercased
+    uppercased_hexdigits = ''.join(set(string.hexdigits.upper()))
+    # 2nd character must be 0, 2, 4, 6, 8, A, C, or E
+    mac = ""
+    for i in range(6):
+        for j in range(2):
+            if i == 0:
+                mac += random.choice("02468ACE")
+            else:
+                mac += random.choice(uppercased_hexdigits)
+        mac += ":"
+    return mac.strip(":")
+
+
+ifaces = get_if_list()
+print(ifaces)
+for iface in ifaces:
     print(iface + ': '+get_if_hwaddr(iface))
 
-    
+
 choice = input('Enter iface choice: ')
 
-is_in = [True for iface in ifaces if iface == choice ] # [True] or []
+is_in = [True for iface in ifaces if iface == choice]  #  [True] or []
 
-if is_in:  #NOTE non vide
+if is_in:  # NOTE non vide
     os.system(f'sudo ifconfig {choice} down')
-    os.system(f'sudo ifconfig {choice} hw ether ')
+    os.system(f'sudo ifconfig {choice} hw ether {get_random_mac_address()} ')
     os.system(f'sudo ifconfig {choice} up')
-    
-
-
